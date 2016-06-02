332ad3920bec15bebb8539e96ebdd58015e9ee53
zxwtry <zxwtry@qq.com>
2016-06-02 19:08:48 +0800

    kekeda

--- dao/StuDao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import vo.Student;
-
-public interface StuDao {
-	public int addStu(Student s);
-	public Student findStu(String xh,String psd);
-}
--- service/StuManager.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import vo.Student;
-
-public interface StuManager {
-	public Student  login(String xh,String psd);
-	public int regist(Student s);
-}
