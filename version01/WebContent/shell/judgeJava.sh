6afb9564e2117e368679ab18d0ef766682def1ab
zxwtry <zxwtry@qq.com>
2017-03-18 22:18:49 +0800

    add WebContent/shell/judgeJava.sh

---
+++ version01/WebContent/shell/judgeJava.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#tempPath="/home/zxwtry/ojpath/shell/$2"
+tempPath=$2
+cd $tempPath
+#in="/home/zxwtry/ojpath/data/poj/$1.input"
+#echo $(pwd) > ttt.txt
+#echo $(java -version) > ppp.txt
+in="$1.input"
+exec < $in
+javac Main.java
+#echo $3 > yyy.txt
+java $3  Main
+#rm Main*
